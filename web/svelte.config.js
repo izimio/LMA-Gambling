04f876cdd053d0284d01483d3860f34ccf8f9c4f
ZiplEix <ZiplEix@users.noreply.github.com>
2024-09-26 14:38:46 +0900

    Mettre à jour les fichiers de configuration Vite et Svelte
    - Mettre à jour le fichier svelte.config.js pour importer les modules 'typescript' et 'vite'
    - Mettre à jour le fichier vite.config.ts pour définir le port du serveur à 3000

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,6 +1,8 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { server } from 'typescript';
+import { defineConfig } from 'vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
